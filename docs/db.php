6d22abecf807ed3f9dbd7b495635507c26070487
JodonyD <jodonyd13@gmail.com>
2025-07-24 18:58:27 -0500

    update

--- docs/db.php
+++ docs/db.php
@@ -1,12 +1,12 @@
-
 <?php
-$servername = "localhost"; // keep as is
-$username = "your_db_username"; // 🔁 replace this
-$password = "your_db_password"; // 🔁 replace this
-$dbname = "your_db_name";       // 🔁 replace this
+$servername = "sql206.byethost10.com";
+$username = "b10_39553702";      
+$password = "Unconditional18!"; 
+$dbname = "b10_39553702_oomas";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
